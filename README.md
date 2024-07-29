548c85f7f71e11d2a56621ef7fc8de3ea6554546
sahil khatri <sahilkeshav1997@gmail.com>
2024-07-29 11:28:38 +0530

    added route structure

--- README.md
+++
@@ -1,4 +0,0 @@
-# COMFY STORE
-
-- Daisy UI, Tailwind Css and packages installation.
-- Added pages (components)
--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,58 @@ import {
   Checkout,
   Orders,
 } from './pages'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: 'products',
+        element: <Products />,
+      },
+      {
+        path: 'products/:id',
+        element: <SingleProduct />,
+      },
+      {
+        path: 'cart',
+        element: <Cart />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: 'checkout',
+        element: <Checkout />,
+      },
+      {
+        path: 'orders',
+        element: <Orders />,
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+    errorElement: <Error />,
+  },
+  {
+    path: '/regiser',
+    element: <Register />,
+    errorElement: <Error />,
+  },
+])
 
 const App = () => {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+  return <RouterProvider router={router} />
 }
 
 export default App
