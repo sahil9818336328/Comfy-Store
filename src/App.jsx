2f66c8282d9eb75234f6b50172da558751de4243
sahil khatri <sahilkeshav1997@gmail.com>
2024-07-29 10:50:33 +0530

    addes pages (components)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
 import React from 'react'
 
+import {
+  HomeLayout,
+  Landing,
+  Error,
+  Products,
+  SingleProduct,
+  Cart,
+  About,
+  Register,
+  Login,
+  Checkout,
+  Orders,
+} from './pages'
+
 const App = () => {
   return <h1 className='text-3xl font-bold underline'>Hello world!</h1>
 }
